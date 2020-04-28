6033b478c9adcfc878d5a6e7edb8f2e8386c46d3
asedlack <asedlackcode@gmail.com>
2020-04-27 21:35:04 -0700

    fixed issues

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const db = require("./db.json");
+
 
 const app = express();
-const PORT = 8000;
+const PORT = 8085;
 
-let note = [
-    {
-        id:1,
-        body:"text in note"
-    },
-    {
-        id:2,
-        body:"the noted text"
-    }
-];
+var notes = [];
 
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
+    
+    res.sendFile(path.join(__dirname, "./index.html"))
 });
 
-app.get("/api/note", function(req, res) {
-    return res.json(note);
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+app.get("/api/notes",function(req,res) {
+    fs.readFile("./db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        } 
+        return res.json(data);
+    });
+});
+
+app.post("/api/notes", function (req, res) {
+    fs.writeFile("./db.json", "utf8")
 })
 
 app.listen(PORT, function() {
